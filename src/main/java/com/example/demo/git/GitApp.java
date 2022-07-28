1967bbff900a5dfac92b78c1c03ad12bcd5d001d
SoumyaL2 <soumyapanda834@gmail.com>
2022-07-28 12:50:51 +0530

    bug fixed for 101

--- src/main/java/com/example/demo/git/GitApp.java
+++ src/main/java/com/example/demo/git/GitApp.java
@@ -7,6 +7,14 @@ import org.springframework.stereotype.Component;
 public class GitApp {
 	//changes for bug 121
 	String s="Luisa";
+
+
+	//making changes for 101
+	int i=10;
+	int j=20;
+	
+
+
 	Boolean b1=Boolean.valueOf("True");
 	float f=687.6f;
 	
